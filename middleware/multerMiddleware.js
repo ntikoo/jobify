47f91617b1692e282bfa72ce516f33256276470c
ntikoo <ntikoo@gmail.com>
2024-01-16 08:04:47 +0530

    console command removed

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,13 +1,12 @@
-import multer from "multer";
-import DataParser from 'datauri/parser.js'
-import path from 'path'
+import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage();
 const parser = new DataParser();
 
-export const formatImage =(file)=>{
-    const fileExtension = path.extname(file.originalName).toString();
-    return parser.format(fileExtension,file.buffer).content
-    console.log(file)
-}
-export const upload = multer({storage})
+export const formatImage = (file) => {  
+  const fileExtension = path.extname(file.originalname).toString(); 
+  return parser.format(fileExtension, file.buffer).content;
+};
+export const upload = multer({ storage });
